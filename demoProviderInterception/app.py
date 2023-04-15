6c96996767dc341b70a3bdd4d533b0565b4f6e69
SEM <sebastian.maischl@ivfp.de>
2023-04-15 22:42:25 +0200

    demoProviderInterception added

---
+++ demoProviderInterception/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import requests
+import redis
+
+app = Flask(__name__)
+
+# pool = redis.ConnectionPool(host='localhost', port=6379, db=0, password="testadmin")
+# redis = redis.Redis(connection_pool=pool)
+
+# redis.set('testkey', 'Hello Meetup')
+    
+# entry = redis.get('testkey')
+# print(entry)
+
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    return "demoProviderInterception"
+
+app.run(debug=True, host='0.0.0.0', port=3000)
